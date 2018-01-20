7d11a2a2f0ca697eb3b61a5c7fd31fc0383c1103
France98 <larn_francisza@hotmail.com>
2018-01-21 00:54:54 +0700

    add JavaDoc and some method

--- src/coinpurse/MoneyUtil.java
+++ src/coinpurse/MoneyUtil.java
@@ -4,13 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Filter currency of coin and sort the coin.
  * 
  * @author Phanuwatch Luangpradit
  *
  */
 public class MoneyUtil {
 
-	public static List<Coin> filterByCurency(List<Coin> coins, String currency) {
+	/**
+	 * Print value of coin and currency that in the list.
+	 * @param coins
+	 */
+	private static void printCoin(List<Coin> coins) {
+		for(int i = 0; i < coins.size(); i++){
+			System.out.println(coins.get(i));
+		}
+	}
+
+	/**
+	 * Filter the coin by using currency
+	 * @param coins
+	 * @param currency
+	 * @return
+	 */
+	public static List<Coin> filterByCurrency(List<Coin> coins, String currency) {
 		List<Coin> same = new ArrayList<>();
 		for (int i = 0; i < coins.size(); i++) {
 			if (coins.get(i).getCurrency().equalsIgnoreCase(currency)) {
@@ -19,9 +36,27 @@ public class MoneyUtil {
 		}
 		return same;
 	}
-	
-	public static void sortCoins(List<Coin> coin){
+
+	/**
+	 * Sort the coin value by using Collections.sort
+	 * @param coin
+	 */
+	public static void sortCoins(List<Coin> coin) {
 		java.util.Collections.sort(coin);
 	}
 	
+	/**
+	 * Test the method.
+	 * @param arg
+	 */
+	public static void main(String[] arg) {
+		List<Coin> coins = new ArrayList<Coin>();
+		coins.add(new Coin(10, "Baht"));
+		coins.add(new Coin(5, "Baht"));
+		coins.add(new Coin(20, "Baht"));
+		coins.add(new Coin(25, "Baht"));
+		coins.add(new Coin(40, "Dollar"));
+		coins = filterByCurrency(coins, "Baht");
+		printCoin(coins);
+	}
 }
