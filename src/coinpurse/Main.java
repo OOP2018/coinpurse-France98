173e5636818c721dfae74d19696d0ee6d6114fa1
France98 <larn_francisza@hotmail.com>
2018-02-15 23:09:20 +0700

    modify the code to lab5

--- src/coinpurse/Main.java
+++ src/coinpurse/Main.java
@@ -13,6 +13,7 @@ public class Main {
      */
     public static void main( String[] args ) {
     	Purse purse = new Purse(10);
+    	MoneyFactory.setMoneyFactory(new MalayMoneyFactory());
     	ConsoleDialog ui = new ConsoleDialog(purse);
     	ui.run();
     }
