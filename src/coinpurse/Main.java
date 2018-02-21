06efb0a803925c5ffde36f1ebfc2290cd6e037b8
France98 <larn_francisza@hotmail.com>
2018-02-21 10:09:31 +0700

    add method name init() that have ResourceBundle

--- src/coinpurse/Main.java
+++ src/coinpurse/Main.java
@@ -1,5 +1,7 @@
 package coinpurse;
- 
+
+import java.util.ResourceBundle;
+
 /**
  * A main class to create objects and connect objects together.
  * The user interface needs a reference to coin purse.
@@ -12,9 +14,31 @@ public class Main {
      * @param args not used
      */
     public static void main( String[] args ) {
+    	init();
     	Purse purse = new Purse(10);
-    	MoneyFactory.setMoneyFactory(new MalayMoneyFactory());
     	ConsoleDialog ui = new ConsoleDialog(purse);
     	ui.run();
+    }
+    
+    public static void init(){
+    	ResourceBundle bundle = ResourceBundle.getBundle( "purse" );
+    	String factoryclass = bundle.getString("moneyfactory");
+    	//TODO if factoryclass is null then use a default class name
+    	MoneyFactory factory = null;
+    	try {
+    	 factory = (MoneyFactory)Class.forName(factoryclass).newInstance();
+    	 MoneyFactory.setMoneyFactory(factory);
+    	}
+    	catch (ClassCastException cce) {
+    	 //the object could not be cast to type MoneyFactory
+    	 System.out.println(factoryclass +" is not type MoneyFactory");
+    	 MoneyFactory.setMoneyFactory(new ThaiMoneyFactory());  //Set as default
+    	}
+    	catch (Exception ex) {
+    	 // any other exception means we could not create an object
+    	 System.out.println("Error creating MoneyFactory "+ ex.getMessage() );
+    	}
+    	// if no factory then quit
+    	if (factory == null) System.exit(1);
     }
 }
